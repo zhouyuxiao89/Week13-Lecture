e03a67929bcd8de38c3c39fc5a3f4c2832a27d30
zhouyuxiao89 <zhouyuxiao89@yahoo.com>
2013-04-29 21:22:39 -0700

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 public class Dice {
+  static int RollCount = 0;
+  public static int RollDice(int DiceNumber,int Max)
+  {
+      int dice;
+        dice = 1 + (int)(Math.random()*Max*DiceNumber);
+          RollCount ++;
+            return dice;	
+  }
+  public int getCount()
+  {
+      return RollCount;
+  }
+  public static int Roll_1_Dice()
+  {
+      int computerNum;
+        computerNum = 1 + (int)(Math.random()*6);
+          return computerNum;
+  }
+}
 
-public int RollDice(int DiceNumber)
-{
-  int computerNum;
-  computerNum = 1 + (int)(Math.random()*6*DiceNumber);
-  return computerNum;
-}
-public static int Roll_1_Dice()
-{
-  int computerNum;
-  computerNum = 1 + (int)(Math.random()*6);
-  return computerNum;
-}
-}
+  }
+  }
+  }
