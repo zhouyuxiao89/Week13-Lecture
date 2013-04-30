bb4d5b14ee539a59e0b2ab218a028bfdad2a521a
zhouyuxiao89 <zhouyuxiao89@yahoo.com>
2013-04-29 21:25:58 -0700

    Update DiceTest.java

--- DiceTest.java
+++ DiceTest.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 public class DiceTest {
-  public static void main(String[] args)
-  {
-  int numberOfDice = 3;
-  Dice dice = new Dice();
-  System.out.printf("For 3 Dices, dice number is %d after roll the dices\n\n ",dice.RollDice(numberOfDice) );
-  System.out.printf("For one Dices, dice number is %d after roll the dices\n\n ",Dice.Roll_1_Dice() );
-  }
+    public static void main(String[] args)
+      {
+          	
+            Scanner input = new Scanner(System.in);
+            System.out.println("How many dices do want to roll?\n");
+            int numberOfDice = input.nextInt();
+            System.out.println("What is the max value of each dice?\n");
+            int maxValue = input.nextInt();
+            int diceNumber= Dice.RollDice(numberOfDice, maxValue);
+            Dice dice1 = new Dice();
+            System.out.printf("The dice number is %d:\n",diceNumber);
+            System.out.printf("The times the dice rolled is %d\n",dice1.getCount());
+                                
+            //-------------------------------------------
+            System.out.println("How many dices do want to roll?\n");
+            int number = input.nextInt();
+            System.out.println("What is the max value of each dice?\n");
+            int max = input.nextInt();
+            int dice_Number= Dice.RollDice(number, max);
+            Dice dice2 = new Dice();
+            System.out.printf("The dice number is %d:\n",dice_Number);
+            System.out.printf("The times the dice rolled is %d:",dice2.getCount());
+      }
 }
